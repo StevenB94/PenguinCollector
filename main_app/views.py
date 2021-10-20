148a50529c390f0ea4bfd870746edbb8a8c1afca
Steven Ballerini <stevenballerini@Stevens-MBP.attlocal.net>
2021-10-19 20:40:06 -0700

    day 1 part 2 models and views

--- main_app/views.py
+++ main_app/views.py
@@ -1,28 +1,21 @@
 from django.shortcuts import render
 
 # Add the following import
-from django.http import HttpResponse
+from .models import Penguin
 
-# Add the Cat class & list and view function below the imports
-class Penguin:  # Note that parens are optional if not inheriting from another class
-  def __init__(self, name, breed, description, age):
-    self.name = name
-    self.breed = breed
-    self.description = description
-    self.age = age
-
-penguins = [
-  Penguin('Mac', 'macaroni', 'yellow', 3),
-  Penguin('Sachi', 'emporer', 'black', 0),
-  Penguin('Raven', 'chin strap', 'brown', 4)
-]
 
 # Define the home view
 def home(request):
-    return HttpResponse('<h1>Hello /ᐠ｡‸｡ᐟ\ﾉ</h1>')
+    return render(request,'home.html')
 
 def about(request):
     return render(request, 'about.html')
 
 def penguins_index(request):
-  return render(request, 'penguins/index.html', { 'penguins': penguins })
+    penguins = Penguin.objects.all()
+    return render(request, 'penguins/index.html', { 'penguins': penguins })
+
+
+def penguins_detail(request, penguin_id):
+      penguin = Penguin.objects.get(id=penguin_id)
+      return render(request, 'penguins/detail.html', { 'penguin': penguin })
