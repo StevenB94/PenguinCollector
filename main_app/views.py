4b7e8120fe4f6bd984be1a92382cf006c1c1fa73
Steven Ballerini <stevenballerini@Stevens-MBP.attlocal.net>
2021-10-19 20:17:35 -0700

    day 1 part 1 done

--- main_app/views.py
+++ main_app/views.py
@@ -3,9 +3,26 @@ from django.shortcuts import render
 # Add the following import
 from django.http import HttpResponse
 
+# Add the Cat class & list and view function below the imports
+class Penguin:  # Note that parens are optional if not inheriting from another class
+  def __init__(self, name, breed, description, age):
+    self.name = name
+    self.breed = breed
+    self.description = description
+    self.age = age
+
+penguins = [
+  Penguin('Mac', 'macaroni', 'yellow', 3),
+  Penguin('Sachi', 'emporer', 'black', 0),
+  Penguin('Raven', 'chin strap', 'brown', 4)
+]
+
 # Define the home view
 def home(request):
     return HttpResponse('<h1>Hello /ᐠ｡‸｡ᐟ\ﾉ</h1>')
 
 def about(request):
     return render(request, 'about.html')
+
+def penguins_index(request):
+  return render(request, 'penguins/index.html', { 'penguins': penguins })
