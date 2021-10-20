3284f8bce78a06a65ca135b7dbf127b5e2a275c6
Steven Ballerini <stevenballerini@Stevens-MBP.attlocal.net>
2021-10-19 21:37:27 -0700

    Day 2 one to many done

--- main_app/models.py
+++ main_app/models.py
@@ -1,5 +1,13 @@
 
 from django.db import models
+from django.urls import reverse
+from datetime import date
+
+MEALS = (
+    ('B', 'Breakfast'),
+    ('L', 'Lunch'),
+    ('D', 'Dinner')
+)
 
 class Penguin(models.Model):
   name = models.CharField(max_length=100)
@@ -9,3 +17,27 @@ class Penguin(models.Model):
 
   def __str__(self):
     return self.name
+
+  def get_absolute_url(self):
+    return reverse('detail', kwargs={'penguin_id': self.id})
+
+
+  def fed_for_today(self):
+        return self.feeding_set.filter(date=date.today()).count() >= len(MEALS)
+
+class Feeding(models.Model):
+  date = models.DateField('feeding date')
+  meal = models.CharField(
+    max_length=1,
+    choices=MEALS,
+    default=MEALS[0][0]
+  )
+  penguin = models.ForeignKey(Penguin, on_delete=models.CASCADE)
+
+  def __str__(self):
+    # Nice method for obtaining the friendly value of a Field.choice
+    return f"{self.get_meal_display()} on {self.date}"
+
+  # change the default sort
+  class Meta:
+    ordering = ['-date']
--- main_app/views.py
+++ main_app/views.py
@@ -1,8 +1,20 @@
-from django.shortcuts import render
-
-# Add the following import
+from django.shortcuts import redirect, render
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from .models import Penguin
+from .forms import FeedingForm
 
+
+class PenguinCreate(CreateView):
+    model = Penguin
+    fields = '__all__'
+
+class PenguinUpdate(UpdateView):
+  model = Penguin
+  fields = ['breed', 'description', 'age']
+
+class PenguinDelete(DeleteView):
+  model = Penguin
+  success_url = '/penguins/'
 
 # Define the home view
 def home(request):
@@ -18,4 +30,18 @@ def penguins_index(request):
 
 def penguins_detail(request, penguin_id):
       penguin = Penguin.objects.get(id=penguin_id)
-      return render(request, 'penguins/detail.html', { 'penguin': penguin })
+      feeding_form = FeedingForm()
+      return render(request, 'penguins/detail.html', { 'penguin': penguin, 'feeding_form': feeding_form })
+
+
+def add_feeding(request, penguin_id):
+    	# create the ModelForm using the data in request.POST
+  form = FeedingForm(request.POST)
+  # validate the form
+  if form.is_valid():
+    # don't save the form to the db until it
+    # has the cat_id assigned
+    new_feeding = form.save(commit=False)
+    new_feeding.penguin_id = penguin_id
+    new_feeding.save()
+    return redirect('detail', penguin_id=penguin_id)
